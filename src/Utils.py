60cb5eac90615863b0c174c71de417c75440a906
Dark <anisri477@gmail.com>
2024-02-11 11:07:22 +0530

    preparation of data

--- src/Utils.py
+++ src/Utils.py
@@ -1,6 +1,6 @@
-def aggregate_level1(df):
-    '''writing a function to get item and store level summary metrics for a specific year'''
+import pandas as pd 
 
+def aggregate_level1(df):
     sale_day_store_level = df.groupby(['Year', 'Month', 'Day', 'store_nbr'], as_index=False)[
         'unit_sales'].agg(['sum', 'count'])
     sale_day_store_level = sale_day_store_level.reset_index().rename(
@@ -16,3 +16,18 @@ def aggregate_level1(df):
 
     return sale_day_store_level, sale_day_item_level,sale_store_item_level
 
+
+def classification(ts):
+    adi = len(ts) / len(ts[ts > 0]) if len(ts[ts > 0]) > 0 else float('inf')
+    cv_squared = (sum(((ts - ts.mean())**2) / len(ts)) / ts.mean())
+    f_type = 'Smooth'
+    if (adi > 1.32 and cv_squared < 0.49):
+        f_type = 'Intermittent'
+    elif (adi > 1.32 and cv_squared > 0.49):
+        f_type = 'Lumpy'
+    elif (adi < 1.32 and cv_squared > 0.49):
+        f_type = 'Erratic'              
+    return adi, cv_squared
+
+def to_dateTime(df):
+    df['date'] = pd.to_datetime(df['date'])
